ab6f42e2dc0674f3a550af6f44ae77a3340caa35
Florian Fittschen <ffittschen@gmail.com>
2016-10-24 13:12:22 +0200

    Add HelloUser class

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,25 @@
+/**
+ * Created by ffittschen on 24/10/2016.
+ */
+public class HelloUser {
+    private String username;
+
+    public HelloUser(String username) {
+        this.username = username;
+    }
+
+    public void greetUser() {
+        StringBuilder stringBuilder = new StringBuilder("Hello ");
+        stringBuilder.append(getUsername());
+        stringBuilder.append("!");
+        System.out.println(stringBuilder.toString());
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,6 +2,8 @@
  * 
  */
 
+import java.util.Scanner;
+
 /**
  * @author Mohsen
  *
@@ -13,6 +15,13 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2016 - how are you? :)");
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Please enter your name: ");
+		String name = reader.nextLine();
+
+		HelloUser helloUser = new HelloUser(name);
+
+		helloUser.greetUser();
 	}
 
 }
